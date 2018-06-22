b8fb090cc8540ce9ff815cc5616bdacceaeb262c
vic lotana <viclotana@gmail.com>
2018-06-23 00:58:39 +0100

    add project logic

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class AddProjects extends Component {
+
+    static defaultProps = {
+        categories: ['Web Design','Web Development', 'Mobile Development']
+    }
+
+    handleSubmit(){
+        console.log('submitted');
+    }
   render() {
+    let categoryOptions = this.props.categories.map(category => {
+        return <option key={category} value="category">{category}</option>
+    });
+
     return (
       <div>
         <h3>Add Project</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Title</label><br />
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label>Category</label><br />
+            <select ref="category">{categoryOptions}</select>
+          </div>
+        </form>    
       </div>
     );
   }

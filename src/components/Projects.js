eefbb579a5660d53dbb69bab911283be799fe40d
vic lotana <viclotana@gmail.com>
2018-06-23 00:15:57 +0100

    comments

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-
+import ProjectItem from './ProjectItem';
 class Projects extends Component {
   render() {
     let projectItems;
     if (this.props.projects){
       projectItems = this.props.projects.map(project => {
-        console.log(project);
+        //console.log(project);
+        return (
+          <ProjectItem project={project} />
+        );
       });
     }
     //console.log(this.props);
     return (
       <div className="Projects">
-        My Projects
-        {this.props.test}
+        {projectItems}
       </div>
     );
   }

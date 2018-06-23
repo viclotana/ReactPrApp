abcc9796b3a02c9904f5e967f14e9b8d9fdb9e7a
vic lotana <viclotana@gmail.com>
2018-06-23 01:16:22 +0100

    going

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 class AddProjects extends Component {
+    constructor(){
+        super();
+        this.state = {
+            newProject:{}
+        }
+    }
 
     static defaultProps = {
         categories: ['Web Design','Web Development', 'Mobile Development']
     }
 
     handleSubmit(e){
-        console.log('submitted');
+        if(this.refs.title.value === ''){
+            alert('Title is required');
+        } else {
+            this.setState({newProject:{
+                title: this.refs.title.value,
+                category: this.refs.category.value
+            }}, function(){
+                console.log(this.state);
+            });
+        }
         e.preventDefault();
     }
   render() {
@@ -18,7 +33,7 @@ class AddProjects extends Component {
     return (
       <div>
         <h3>Add Project</h3>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <div>
             <label>Title</label><br />
             <input type="text" ref="title" />
